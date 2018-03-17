786a3bd3a432b331ea325b16c63a134ef208c9d3
AndreyMolochko <Molochko_AG_16@student.grsu.by>
2018-03-17 23:55:48 +0300

    layouts support,thems,languages

---
+++ app/src/main/java/com/example/user/myprogress/FragmentLanguage.java
@@ -0,0 +1,21 @@
+package com.example.user.myprogress;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by User on 17.03.2018.
+ */
+
+public class FragmentLanguage extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frag_language,null);
+        return view;
+    }
+}
